bd28cf61abf8084ad95b3678fd52107f4bedff08
Chukwuka Emi <emichukwuka@gmail.com>
2020-07-26 07:33:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -11,69 +11,71 @@ import "./App.css";
 import CustomersContext from "./Context/Context";
 
 function App() {
-	const [count, setCount] = React.useState("");
-	const [token, setToken] = React.useState(localStorage.getItem("token") || "");
-	const [id, setId] = React.useState(null);
-	const [name, setName] = React.useState(localStorage.getItem("name" || ""));
+  const [count, setCount] = React.useState("");
+  const [token, setToken] = React.useState(localStorage.getItem("token") || "");
+  const [id, setId] = React.useState(null);
+  const [name, setName] = React.useState(localStorage.getItem("name" || ""));
 
-	const login = (id, name, token, tokenExpiration) => {
-		setToken(token);
-		setId(id);
-		setName(name);
-	};
-	const logout = () => {
-		setToken("");
-		setId("");
-		setName("");
-		localStorage.removeItem("token", "");
-	};
+  const login = (id, name, token, tokenExpiration) => {
+    setToken(token);
+    setId(id);
+    setName(name);
+  };
+  const logout = () => {
+    setToken("");
+    setId("");
+    setName("");
+    localStorage.removeItem("token", "");
+  };
 
-	const addCustomer = (count) => {
-		setCount(count);
-	};
+  const addCustomer = (count) => {
+    setCount(count);
+  };
 
-	React.useEffect(() => {
-		localStorage.setItem("token", token);
-		localStorage.setItem("name", name);
-		addCustomer();
-	}, [token]);
+  React.useEffect(() => {
+    localStorage.setItem("token", token);
+    localStorage.setItem("name", name);
+    addCustomer();
+  }, [token, name]);
 
-	return (
-		<BrowserRouter>
-			<CustomersContext.Provider
-				value={{
-					customerCount: count,
-					addCustomer: addCustomer,
-					login: login,
-					logout: logout,
-					token: token,
-					id: id,
-					name: name,
-				}}>
-				<div className="container-fluid justify-content-center">
-					<NavBar logout={logout} count={count} />
-					<main
-						style={{ paddingRight: "15%" }}
-						role="main"
-						className="container">
-						<Switch>
-							<Route exact path="/" component={Home} />
-							{token && <Redirect from="/signin" to="/" />}
-							<Redirect from="/signout" to="/" />
-							{token && <Route path="/customers" component={Customers} />}
-							{token && <Route path="/add-customer" component={AddCustomer} />}
-							<Route path="/signup" component={SignUp} />
-							{token && <Route path="/staff" component={StaffList} />}
-							<Route path="/signin" component={SignIn} />
+  return (
+    <BrowserRouter>
+      <CustomersContext.Provider
+        value={{
+          customerCount: count,
+          addCustomer: addCustomer,
+          login: login,
+          logout: logout,
+          token: token,
+          id: id,
+          name: name,
+        }}
+      >
+        <div className="container-fluid justify-content-center">
+          <NavBar logout={logout} count={count} />
+          <main
+            style={{ paddingRight: "15%" }}
+            role="main"
+            className="container"
+          >
+            <Switch>
+              <Route exact path="/" component={Home} />
+              {token && <Redirect from="/signin" to="/" />}
+              <Redirect from="/signout" to="/" />
+              {token && <Route path="/customers" component={Customers} />}
+              {token && <Route path="/add-customer" component={AddCustomer} />}
+              <Route path="/signup" component={SignUp} />
+              {token && <Route path="/staff" component={StaffList} />}
+              <Route path="/signin" component={SignIn} />
 
-							{!token && <Redirect from="/customers" to="/signin" />}
-							{!token && <Redirect from="/add-customer" to="/signin" />}
-						</Switch>
-					</main>
-				</div>
-			</CustomersContext.Provider>
-		</BrowserRouter>
-	);
+              {!token && <Redirect from="/customers" to="/signin" />}
+              {!token && <Redirect from="/add-customer" to="/signin" />}
+            </Switch>
+          </main>
+        </div>
+      </CustomersContext.Provider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
